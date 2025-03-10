8d83553062b07de996e98542bd2441367d87e13e
Sara <sara.rebecca360@gmail.com>
2025-03-10 23:28:29 +0100

    Add UI

--- script.js
+++ script.js
@@ -6,7 +6,60 @@ const winningCouples = [
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
+const results = document.querySelector(".results");
+
+const choices = document.querySelectorAll(".choice");
+for (choice of choices) {
+  choice.addEventListener("click", (e) => {
+    results.innerHTML = "";
+
+    // Store player choice
+    let playerSelection = e.target.innerText.toLowerCase();
+    playerSelection =
+      playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
+    // Computer choice
+    const computerSelection = getComputerChoice();
+
+    // Display choices
+    const playerChoicePara = document.createElement("p");
+    playerChoicePara.innerText = `Player choice: ${playerSelection}`;
+
+    const computerChoicePara = document.createElement("p");
+    computerChoicePara.innerText = `Computer choice: ${computerSelection}`;
+
+    results.appendChild(playerChoicePara);
+    results.appendChild(computerChoicePara);
+    // Play round
+    resultText = playRound(computerSelection, playerSelection);
+    const resultPara = document.createElement("p");
+    resultPara.appendChild(resultText);
+    resultPara.setAttribute("style", "font-weight:800; font-size:24px");
+    results.appendChild(resultPara);
+
+    let scoresPara = document.createElement("p");
+    scoresPara.innerText = `Player's score: ${humanScore}   Computer's score: ${computerScore}`;
+    results.appendChild(scoresPara);
+    console.log("hs" + humanScore + " cs" + computerScore);
+    if (humanScore === 5 || computerScore === 5) {
+      let winner = document.createElement("p");
+      if (humanScore === 5) {
+        winner.innerText = `The winner is... YOU!`;
+      } else if (computerScore === 5) {
+        winner.innerText = `The winner is... THE COMPUTER!`;
+      }
+      winner.setAttribute(
+        "style",
+        "font-size:28px; font-weight:900; color: red"
+      );
+      results.appendChild(winner);
+      humanScore = 0;
+      computerScore = 0;
+    }
+  });
+}
+
+//playGame();
 
 // Randomly return one of; 'Rock', 'Paper', 'Scissors'
 function getComputerChoice() {
@@ -48,23 +101,33 @@ function getHumanChoice() {
 function playRound(computerChoice, humanChoice) {
   let choiceCouple = [computerChoice, humanChoice];
   if (computerChoice == humanChoice) {
+    const resultText = document.createTextNode("Tie!");
     console.log("Tie!");
-    return;
+
+    return resultText;
   }
 
   for (let i = 0; i < winningCouples.length; i++) {
     if (winningCouples[i].toString() === choiceCouple.toString()) {
+      const resultText = document.createTextNode(
+        `You lose! ${computerChoice} beats ${humanChoice}.`
+      );
+
       console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
       computerScore++;
-      return;
+      return resultText;
     }
   }
+  const resultText = document.createTextNode(
+    `You win! ${humanChoice} beats ${computerChoice}.`
+  );
   console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
   humanScore++;
-  return;
+
+  return resultText;
 }
 
-// Calls playROund five times and declares the winner at the end
+/* // Calls playRound five times and declares the winner at the end
 function playGame() {
   // For five rounds
   for (let i = 0; i < 5; i++) {
@@ -83,4 +146,4 @@ function playGame() {
   } else {
     console.log("It's a tie!");
   }
-}
+} */
